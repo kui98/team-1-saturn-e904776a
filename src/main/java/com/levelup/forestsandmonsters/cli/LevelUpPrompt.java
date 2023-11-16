89180274a3d42ce36c7c4cb7a1b372d3bcb1680b
Laura Smith <laura3babies@yahoo.com>
2023-11-16 17:20:23 -0500

    Partial refactor of UI

--- src/main/java/com/levelup/forestsandmonsters/cli/LevelUpPrompt.java
+++ src/main/java/com/levelup/forestsandmonsters/cli/LevelUpPrompt.java
@@ -10,7 +10,7 @@ public class LevelUpPrompt implements PromptProvider {
     @Override
     public AttributedString getPrompt() {
         return new AttributedString(
-                "Enter command" + "==> ",
+                "Enter option :",
                 AttributedStyle.DEFAULT.background(AttributedStyle.GREEN));
     }
 }
